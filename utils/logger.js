7ecfa9f11441848c82e80e15d3c77a1d851acdfd
Mohana Misra <iammohanamisra@gmail.com>
2024-01-18 14:26:31 +0530

    refactored

---
+++ utils/logger.js
@@ -0,0 +1,12 @@
+const info = (...params) => {
+    if(process.env.NODE_ENV != 'TEST')
+        console.log(...params);
+}
+const error = (...params) => {
+    if(process.env.NODE_ENV != 'TEST')
+        console.log(...params);
+}
+
+module.exports = {
+    info, error
+}
